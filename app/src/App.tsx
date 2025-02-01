4afec2aa691f42fd97025c11389d09060756569e
sebastianopderbeck <opderbeck@parsimotion.com>
2025-02-01 22:36:51 +0100

    fix: react problem on app.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {TaskList} from "./task/taskList.tsx";
 import {TaskProvider} from "./task/taskProvider.tsx";
