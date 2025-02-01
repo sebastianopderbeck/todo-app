c5b9627d38a1d804bd4918194f5deb9e07616778
sebastianopderbeck <opderbeck@parsimotion.com>
2025-02-02 00:53:20 +0100

    feat: making services for tasks

---
+++ app/src/services/taskService.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { Task } from "../types/taskTypes";
+
+const API_URL = "http://localhost:8000/tasks";
+
+export const getTasks = async (): Promise<Task[]> => {
+    const response = await axios.get<Task[]>(API_URL);
+    return response.data;
+};
+
+export const createTask = async (task: Omit<Task, "id">): Promise<Task> => {
+    const response = await axios.post<Task>(API_URL, task);
+    return response.data;
+};
+
+export const updateTask = async (updatedTask: Task): Promise<Task> => {
+    const response = await axios.put<Task>(`${API_URL}/${updatedTask.id}`, updatedTask);
+    return response.data;
+};
+
+export const deleteTask = async (id: string): Promise<void> => {
+    await axios.delete(`${API_URL}/${id}`);
+};
