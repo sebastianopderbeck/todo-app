ea84d81061506907c400e7cca16b27e560a953b7
sebastianopderbeck <opderbeck@parsimotion.com>
2025-02-02 12:35:15 +0100

    fix: database name

--- server/database.py
+++ server/database.py
@@ -2,9 +2,11 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Cargar variables de entorno desde .env
+load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://mongo:27017/")
+DB_NAME = os.getenv("DB_NAME", "task_db")
+
 client = MongoClient(MONGO_URL)
-db = client["todo_db"]  # Base de datos
-tasks_collection = db["tasks"]  # Colección de tareas
+db = client[DB_NAME]
+tasks_collection = db["tasks"]
